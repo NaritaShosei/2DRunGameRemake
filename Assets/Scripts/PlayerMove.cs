609a0dacc18ed8de09a1c7d529c0c4ba936f9f7b
NaritaShosei <onigirikun7171@gmail.com>
2024-10-05 15:13:02 +0900

    Playerの移動

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed;
+    Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 vel = _rb.velocity;
+        vel.x = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+        vel.y = Input.GetAxisRaw("Vertical") * _moveSpeed;
+        _rb.velocity = vel;
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, -2.7f, 2.7f);
+        pos.y = Mathf.Clamp(transform.position.y, -4.5f, 4.5f);
+        transform.position = pos;
+    }
+}
