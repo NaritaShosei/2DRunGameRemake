7e2686b73e7d6c8709064034276c8e06e3a5bbc8
NaritaShosei <onigirikun7171@gmail.com>
2024-10-14 21:33:32 +0900

    以下同文

--- Assets/Scripts/DistanceChangeSlider.cs
+++ Assets/Scripts/DistanceChangeSlider.cs
@@ -5,20 +5,13 @@ using UnityEngine.UI;
 
 public class DistanceChangeSlider : MonoBehaviour
 {
-<<<<<<< HEAD
     public static float _distance = 500;
     public static float _max;
-=======
-    public static float _distance;
->>>>>>> 25c19ca3a0032357a14ab0cd91b65e8a6282369e
     Slider _slider;
     void Start()
     {
         _slider = GetComponent<Slider>();
-<<<<<<< HEAD
         _max = _slider.maxValue;
-=======
->>>>>>> 25c19ca3a0032357a14ab0cd91b65e8a6282369e
     }
 
     // Update is called once per frame
